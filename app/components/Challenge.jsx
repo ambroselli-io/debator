a0335a36558e4224c9167a64999346be855b3849
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2022-06-13 11:18:09 +0200

    title on link

--- app/components/Challenge.jsx
+++ app/components/Challenge.jsx
@@ -24,6 +24,7 @@ const Challenge = ({ challenge, hideQuestionMark = false }) => (
           <>
             <a
               href="#challenge-description"
+              title="Cliquez sur ce lien qui vous dirigera vers l'explication en bas de page. Si rien ne se passe, regardez en bas de la page, l'explication est déjà là !"
               className="ml-2 flex h-6 w-6 shrink-0 items-center justify-center rounded-full border border-app text-sm text-app"
             >
               ?
--- app/db/methods/challenge-format.server.js
+++ app/db/methods/challenge-format.server.js
@@ -1,7 +1,6 @@
 import { removeDiacritics } from "app/services/formatSearch.server";
 
 export const challengeFormat = (challenge) => {
-  console.log("challengeFormat", challenge);
   return {
     _id: challenge._id,
     title: removeDiacritics(challenge.title),
